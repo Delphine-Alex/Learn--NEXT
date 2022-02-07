70e44bca55fd1c54df7eb9eac2bab9423a0ffe09
Delphine <delphine.pva@gmail.com>
2022-02-07 11:44:05 +0100

    React struture with sass

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Mainlayout = () => {
+    return (
+        <div>
+            <header>
+
+            </header>
+            <main>
+
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    );
+}
+
+export default Mainlayout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import TitlePage from "../components/TitlePage";
-import Contact from "../components/Contact";
 
 export default function Home() {
   return (
     <div className="home_page">
       < TitlePage title="My homepage" />
-      < Contact title="Contact us" />
     </div>
   )
 }
